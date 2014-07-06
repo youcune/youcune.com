f0b27f6406a3ccc691afa3e8a0d484f77383708f
youcune <you@nakanishi.email>
2014-07-06 13:24:23 +0900

    converted from erb to haml removed qiita_posts

--- config.rb
+++ config.rb
@@ -77,6 +77,16 @@ end
 # end
 
 helpers do
+  def icon(icon, text="", html_options={})
+    content_class = "fa fa-#{icon}"
+    content_class << " #{html_options[:class]}" if html_options.key?(:class)
+    html_options[:class] = content_class
+
+    html = content_tag(:i, nil, html_options)
+    html << " #{text}" unless text.blank?
+    html.html_safe
+  end
+
   def format_date(date)
     date.strftime('%Y.%m.%d')
   end
