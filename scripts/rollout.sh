25dfdf8596fd3d6d1106521a4aa80bf75e83b3ee
youcune <you@nakanishi.email>
2014-12-19 15:32:17 +0900

    added bundle install task

--- scripts/rollout.sh
+++ scripts/rollout.sh
@@ -11,6 +11,7 @@ git fetch
 git checkout $BRANCH
 git merge --ff origin/$BRANCH
 
+bundle install --path ../shared/bundle --jobs=4
 bundle exec middleman build
 
 curl 'http://www.google.com/webmasters/sitemaps/ping?sitemap=https%3A%2F%2Fyoucune.com%2Fsitemap.xml'
